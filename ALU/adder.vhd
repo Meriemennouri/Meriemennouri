f4bc9608a01ed291aee090b31cc6ab29b9b15b2f
thamugadi <qirimhangli@gmail.com>
2023-01-04 04:32:45 +0100

    (renommage)

--- ALU/adder.vhd
+++ ALU/adder.vhd
@@ -2,13 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-entity adder16 is
+entity adder is
     port (
         a, b: in std_logic_vector(15 downto 0);
         sub: out std_logic_vector(15 downto 0);
         c_out: out std_logic
     );
-end adder16;
+end adder;
 
 architecture dataflow of adder16 is
 begin
