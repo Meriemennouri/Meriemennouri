a4ceb4ffad36b5fbf554f166397014319675104e
thamugadi <qirimhangli@gmail.com>
2023-01-04 04:30:09 +0100

    Additionneur 16-bit (avec retenue)

---
+++ ALU/adder.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity adder16 is
+    port (
+        a, b: in std_logic_vector(15 downto 0);
+        sub: out std_logic_vector(15 downto 0);
+        c_out: out std_logic
+    );
+end adder16;
+
+architecture dataflow of adder16 is
+begin
+    sub <= a xor b;
+    c_out <= '1' when (a < b) else '0';
+end dataflow;
+
