2fdb7998f8026f568a04423443eff09d4a53b604
Ennouri meriem <95147317+Meriemennouri@users.noreply.github.com>
2023-01-04 15:49:14 +0100

    Update implémentation_mémoire_vivre.vhdl
    Signed-off-by: Ennouri meriem <95147317+Meriemennouri@users.noreply.github.com>

--- ALU/implémentation_mémoire_vivre.vhdl
+++ ALU/implémentation_mémoire_vivre.vhdl
@@ -1,17 +1,47 @@
--- dans la partie déclarative de l’architecture
-type memoireDonnees_type is array(0 to 2 ** Md - 1) of signed(Wd - 1 downto 0);
-signal memoireDonnees : memoireDonnees_type;
-signal sortieMemoireDonnees : signed(Wd - 1 downto 0);
-signal adresseMemoireDonnees : integer range 0 to 2 ** Md - 1;
-signal lectureEcritureN : std_logic;
--- dans le corps de l’architecture
--- mémoire des données
-process (CLK)
+entity RAMregistres is
+Port ( add_r : in STD_LOGIC;
+add_w : in STD_LOGIC;
+write_enable : in STD_LOGIC;
+H: in STD_LOGIC;
+reset : in STD_LOGIC;
+data_w : in STD_LOGIC_VECTOR (15 downto 0);
+data_r : out STD_LOGIC_VECTOR (15 downto 0));
+end RAM_2registres;
+
+architecture Behavioral of RAMregistres is
+
+signal mem0 : std_logic_vector(15 downto 0);
+signal mem1 : std_logic_vector(15 downto 0);
+
 begin
-if rising_edge(CLK) then
-if lectureEcritureN = '0' then
-memoireDonnees(adresseMemoireDonnees) <= B;
+
+process(H)
+
+begin
+
+if (H'event and H='1') then
+if (reset='1') then
+mem0<=x"0000";
+mem1<=x"0000";
+data_r<=x"0000";
+else
+
+--lecture
+if (add_r='0') then data_r<=mem0;
+else data_r<=mem1;
+end if;
+
+--ecriture
+if (write_enable='1') then
+if (add_w='0') then mem0<=data_w;
+else mem1<=data_w;
 end if;
 end if;
+
+end if;
+end if;
+
 end process;
-sortieMemoireDonnees <= memoireDonnees(adresseMemoireDonnees);
+
+
+end Behavioral;
