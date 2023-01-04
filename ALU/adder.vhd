2d4f0ae90a2f2a05afd92f41d0c66da96e580aa0
thamugadi <qirimhangli@gmail.com>
2023-01-04 04:37:19 +0100

    correction

--- ALU/adder.vhd
+++ ALU/adder.vhd
@@ -5,14 +5,14 @@ use ieee.std_logic_unsigned.all;
 entity adder is
     port (
         a, b: in std_logic_vector(15 downto 0);
-        sub: out std_logic_vector(15 downto 0);
+        res : out std_logic_vector(15 downto 0);
         c_out: out std_logic
     );
 end adder;
 
 architecture dataflow of adder is
 begin
-    sub <= a xor b;
-    c_out <= '1' when (a < b) else '0';
+    res <= a + b;
+    c_out <= '1' when res(15) = '1' else '0';
 end dataflow;
 
