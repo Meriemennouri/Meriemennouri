3cb4b10563c9ddbf33e571bb8c6213f839553a95
thamugadi <qirimhangli@gmail.com>
2023-01-09 18:38:10 +0100

    12 bits

--- memory/ram.vhdl
+++ memory/ram.vhdl
@@ -4,8 +4,8 @@ use IEEE.std_logic_unsigned.all;
 
 entity ram_16k is
     Port ( clk : in  std_logic;
-           io : in  std_logic; -- 
-           address : in  std_logic_vector(15 downto 0);
+           io : in  std_logic; -- 0 : read, 1 : write 
+           address : in  std_logic_vector(11 downto 0);
            ram_in : in  std_logic_vector(15 downto 0);
            ram_out : out  std_logic_vector(15 downto 0));
 end ram_16k;
