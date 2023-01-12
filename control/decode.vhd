bb7f9e2b624cb652ee7361aef4925ecb043f5ad9
Ennouri meriem <95147317+Meriemennouri@users.noreply.github.com>
2023-01-12 23:23:05 +0100

    Update decode.vhd
    Signed-off-by: Ennouri meriem <95147317+Meriemennouri@users.noreply.github.com>

--- control/decode.vhd
+++ control/decode.vhd
@@ -5,18 +5,19 @@ port(
 A : in std_logic_vector(3 downto 0);
 F: out std_logic_vector(11 downto 0)
 );
-end decodeur38;
-architecture flotDeDonnees of decodeur38 is
+end decodeur4_12;
+architecture flotDeDonnees of decodeur 4_12 is
 begin
 with A select F <=
-"00000001" when "000",
-"00000010" when "001",
-"00000100" when "010",
-"00001000" when "011",
-"00010000" when "100",
-"00100000" when "101",
-"01000000" when "110",
-"10000000" when "111",
+"000000001" when "0000",
+"000000010" when "0001",
+"000000100" when "0010",
+"000001000" when "0011",
+"000010000" when "0100",
+"000100000" when "0101",
+"001000000" when "0110",
+"010000000" when "0111",
+"110000000" when "1111",
 (others => 'X') when others;
 end flotDeDonnees;
 
